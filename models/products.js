e9ee9b040d7440be282452c2be7ec5e44e21e3b8
lee <higgs95@naver.com>
2022-04-11 07:17:54 +0000

    [ADD] modeling

--- models/products.js
+++ models/products.js
@@ -1,52 +1,50 @@
 const sequelize = require('sequelize');
 
 class Product extends Model {
-    static init(sequelize, DataTypes){
-        return super.init({
-            name:{
-                type: DataTypes.STRING(50),
-                allowNull: false,
-                unique: true
-            },
-            description: {
-                type: DataTypes.TEXT(50),
-                allowNull: true
-            },
-            category:{
-                primaryKey: true
-            }
-        })
-    }
+  static init(sequelize, DataTypes) {
+    return super.init({
+      name: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+        unique: true,
+      },
+      description: {
+        type: Sequelize.TEXT(50),
+        allowNull: true,
+      },
+      category: {
+        primaryKey: true,
+      },
+    });
+  }
 }
 
 class Option extends Model {
-    static init(sequelize, DataTypes){
-        return super.init({
-            name:{
-                type: DataTypes.STRING(50),
-                allowNull: false,
-                unique: true
-            }
-        })
-    }
+  static init(sequelize, DataTypes) {
+    return super.init({
+      name: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+        unique: true,
+      },
+    });
+  }
 }
 
 class ProductOption extends Model {
-    static init(sequelize, DataTypes){
-        return super.init({
-            option:{
-                primaryKey: true
-            },
-            product:{
-                primaryKey: true
-            }
-        })
-    }
+  static init(sequelize, DataTypes) {
+    return super.init({
+      option: {
+        primaryKey: true,
+      },
+      product: {
+        primaryKey: true,
+      },
+    });
+  }
 
-    static associate(db){
-        db.Product.belongsToMany(Option, {through: 'ProductOption'});
-        db.Option.belongsToMany(Product, {through: 'ProductOption'});
-    }
+  static associate(db) {
+    db.Product.belongsToMany(Option, { through: 'ProductOption' });
+    db.Option.belongsToMany(Product, { through: 'ProductOption' });
+  }
 }
-
-
